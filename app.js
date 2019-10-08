8f4aae283bf49477418bcfc404c09e7dc9526614
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-10-08 09:33:46 +0100

    Connected routes to app and  passed json data.

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import validateRoute from './routes/api/data';
+import removeInputRoute from './routes/api/input';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use('/api', validateRoute);
+app.use('/api/remove', removeInputRoute);
 
 app.use('/', (req, res) => {
   res.status(200).send('Welcome to the Natter Base API.');
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
