4406463e050de58f97d57ee3c6da64b8dccfdbd6
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-10-09 07:44:14 +0100

    Removed port due to tests failing because of it.

--- app.js
+++ app.js
@@ -17,9 +17,5 @@ app.use('/', (req, res) => {
   res.status(200).send('Welcome to the Natter Base API.');
 });
 
-const port = process.env.PORT || 8000;
-app.listen(port,
-  () =>
-  console.log(`Server running on http://localhost:${port}`)
-);
+
 module.exports = app;
